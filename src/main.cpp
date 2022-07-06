abd2f32983ae76341a61db816723461782152158
Thatcher Chen <thatcherchen@163.com>
2022-07-06 19:57:50 +0800

    Update some ideas.

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,12 @@
 
 // Still Designing...
 
+// I hava some better ideas on this project's structure
+
+// I guess I will begin this project after finishing reading <<A Tour of C++>>, because I found that it gave me abundant
+// instructions on how to code better in C++.
+
 int main(int argc, char* argv[])
 {
-
     return 0;
 }
